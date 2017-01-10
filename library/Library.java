e5e42f7804689effd0bf52ac5947e2373ec0bee8
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2017-01-10 17:25:18 +0000

    just got the basic library file set up and have written the first test

---
+++ library/Library.java
@@ -0,0 +1,19 @@
+package library;
+
+import library;
+import java.util.*;
+
+public class Library {
+
+  private ArrayList< Enjoyable > library;
+  private ArrayList< Enjoyable > audio;
+  private ArrayList< Enjoyable > video;
+
+  public Library() {
+    this.library = new ArrayList< Enjoyable >();
+    this.audio = new ArrayList< Enjoyable >();
+    this.video = new ArrayList< Enjoyable >();
+  }
+
+
+}
---
+++ specs/LibraryTest.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.*;
+import java.util.*;
+import org.junit.*;
+import library.*;
+
+public class LibraryTest {
+
+  Audio album;
+  Video video;
+  Library library;
+
+  @Before
+  public void before() {
+    album = new Audio( "Skinny Pigeon", "The Mice Will Pay", "2007" );
+    video = new Video( "Jaws", "Steven Spielberg", "1979" );
+    library = new Library();
+  }
+
+  @Test
+  public void libraryCanAddMedia() {
+    library.addMedia( album );
+    assertEquals( 1, library.displayLibrarySize() );
+  }
+
+}
