e95db17d907400061883e05c2683e65b24d723a8
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2017-01-10 17:27:27 +0000

    looks like it's working nicely, will begin to add more complexity to it

--- library/Library.java
+++ library/Library.java
@@ -1,6 +1,6 @@
 package library;
 
-import library;
+import library.*;
 import java.util.*;
 
 public class Library {
@@ -13,6 +13,14 @@ public class Library {
     this.library = new ArrayList< Enjoyable >();
     this.audio = new ArrayList< Enjoyable >();
     this.video = new ArrayList< Enjoyable >();
+  }
+
+  public void addMedia( Enjoyable media ) {
+    this.library.add( media );
+  }
+
+  public int displayLibrarySize() {
+    return this.library.size();
   }
 
 
